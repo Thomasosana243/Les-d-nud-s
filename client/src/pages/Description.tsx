67b171b26002528d311419140bc46ce29088edc3
michaelgirardet <michaelgirardet@outlook.com>
2024-12-05 22:46:57 +0100

    avant merge dev

--- client/src/pages/Description.tsx
+++ client/src/pages/Description.tsx
@@ -1,15 +1,35 @@
-import NavBar from "../components/NavBar/NavBar";
-
-const Description = () => {
+import { useState, useEffect } from "react";
+function Description() {
+	const [element, setElement] = useState<any>(null);
+	useEffect(() => {
+		fetch("http://localhost:3310/naturiste")
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				setElement(data[0]);
+			})
+			.catch((error) =>
+				console.error("Erreur de chargement des données :", error),
+			);
+	}, []);
+	if (!element) {
+		return <p>Loading...</p>;
+	}
 	return (
-		<section>
-			<NavBar />
-			<div>
-				<h2 style={{ backgroundColor: "red" }}>Description du projet</h2>
-				<p>Voici la description de mon projet React avec TypeScript.</p>
-			</div>
-		</section>
+		<div>
+			<h2 style={{ backgroundColor: "purple", color: "white" }}>
+				Description du projet
+			</h2>
+			<p>{element.description}</p>
+			<p>
+				<strong>Titre: </strong>
+				{element.title}
+			</p>
+			<p>
+				<strong>Autre Info: </strong>
+				{element.otherInfo}
+			</p>
+		</div>
 	);
-};
-
+}
 export default Description;
