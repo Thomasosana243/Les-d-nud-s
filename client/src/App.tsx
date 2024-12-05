920150a13dbf52fb8cb1654d9cb7dfdbca56bc55
Daniel Stacoffe <daniel.stacoffe@wanadoo.fr>
2024-12-05 22:27:47 +0100

    filtre region

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,21 @@
 import { useState, useEffect } from "react";
+import Location from "../components/Filters/Location";
 
 import "./App.css";
 
 function App() {
-	const [photos, setPhotos] = useState([]);
-	useEffect(() => {
-		fetch("http://localhost:3310/naturiste")
-			.then((res) => res.json())
-			.then((data) => console.log(data));
-	}, []);
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3310/naturiste")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
 
-	return <></>;
+  return (
+    <>
+      <Location />
+    </>
+  );
 }
 
 export default App;
