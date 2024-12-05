adfb4c3e788bdd1ffb438af9573114a2844415e8
Thomasosana243 <thomasjosueosana@gmail.com>
2024-12-05 15:20:40 +0100

    maj

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,28 @@
 import { useState, useEffect } from "react";
-
+import { Link } from "react-router-dom";
 import "./App.css";
+import Description from "./pages/Description";
 
 function App() {
-	const [photos, setPhotos] = useState([]);
-	useEffect(() => {
-		fetch("http://localhost:3310/naturiste")
-			.then((res) => res.json())
-			.then((data) => console.log(data));
-	}, []);
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3310/naturiste")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
 
-	return <></>;
+  return (
+    <>
+      <div>
+        <Description />
+        <ul>
+          <li>
+            <Link to="/description">Voir la Description</Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
